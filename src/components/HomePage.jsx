84845654d4a2527a170d90092e89910f85e40027
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-07-31 09:20:43 -0300

    feat: validations

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import ContactList from "./ContactsList";
 
 const HomePage = () => {
-  const contacts = [
+  const [contacts, setContacts] = useState([
     {
       id: 1,
       name: "João",
@@ -17,12 +17,44 @@ const HomePage = () => {
       email: "guilherme@teste.com",
       image: "guilherme.jpg",
     },
-  ];
+  ]);
 
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleAddContact = () => {
+    const newContact = {
+      id: contacts.length + 1,
+      name: "New Contact",
+      contact: "987654321",
+      email: "newcontact@test.com",
+      image: "default.jpg",
+    };
+
+    if (newContact.name.length <= 5) {
+      alert("Name should be a string with more than 5 characters.");
+      return;
+    }
+
+    if (newContact.contact.length !== 9 || isNaN(newContact.contact)) {
+      alert("Contact should be a 9-digit number.");
+      return;
+    }
+
+    if (!isValidEmail(newContact.email)) {
+      alert("Invalid email address.");
+      return;
+    }
+
+    setContacts([...contacts, newContact]);
+  };
   return (
     <div>
       <h1>Contacts List</h1>
       <ContactList contacts={contacts} />
+      <button onClick={handleAddContact}>Add Contact</button>
     </div>
   );
 };
